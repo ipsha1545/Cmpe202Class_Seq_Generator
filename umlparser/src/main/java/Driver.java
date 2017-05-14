e2d9dfeeb519136f33c1d0c96bca999ebf1e35d6
Ipsha Mohanty <ipsha1545@gmail.com>
2017-05-13 20:46:38 -0700

    Restructuring code
    Restructuring code

--- umlparser/src/main/java/Driver.java
+++ umlparser/src/main/java/Driver.java
@@ -1,14 +1,20 @@
 
-
 public class Driver {
 
     public static void main(String[] args) throws Exception {
-       if (args[0].equals(("seq"))) {
+      if (args[0].equals("class")) {
+            EngineParser pe = new EngineParser(args[1],args[2]);
+            pe.start();
+        } else if (args[0].equals(("seq"))) {
         	EngineParserSeq pse = new EngineParserSeq(args[1], args[2], args[3], args[4]);
             pse.process();
         } else {
             System.out.println("Invalid keyword " + args[0]);
         }
+    	
+    /*	//args[0] = "seq";
+    	EngineParserSeq pse = new EngineParserSeq("/Users/ipshamohanty/Desktop/Test Classes/uml-sequence-test","Optimist" ,"update", "diagram");
+        pse.process();*/
     	
     }
 
